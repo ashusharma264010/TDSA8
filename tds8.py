dd9e650a18190f9f973c35289e845b168ebe9ac2
Ashu Sharma <68197002+ashusharma264010@users.noreply.github.com>
2023-04-19 22:39:53 +0530

    Update tds8.py

--- tds8.py
+++ tds8.py
@@ -5,7 +5,8 @@ import streamlit as st
 st.write("Largest of three numbers")
 
 def main():
-    st.title("Find the largest number") st.write("Enter three numbers and click the button to find the largest number")
+    st.title("Find the largest number") 
+    st.write("Enter three numbers and click the button to find the largest number")
     num1 = st.number_input("Enter the first number:")
     num2 = st.number_input("Enter the second number:")
     num3 = st.number_input("Enter the third number:")
@@ -13,13 +14,13 @@ def main():
     if st.button("Find the largest number"):
         if numl > num2 and num1 > num3:
 
-            largest num = numl
+            largest_num = numl
         elif num2 > numl and num2 > num3:
 
             largest_num = num2
 
         else:
-            largest num = num3
+            largest_num = num3
 
         st.write("The largest number is:", largest_num)
 
